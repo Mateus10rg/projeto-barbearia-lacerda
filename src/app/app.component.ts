91ba50f913b0c62ba36f19a2eab51f06c79b15c8
Mateus Rodrigues Gomes <mateus.rg01@gmail.com>
2026-01-22 01:19:51 -0300

    Movendo projeto para a raiz

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
+import { HeroComponent } from './components/hero/hero.component';
+import { ServicesComponent } from './components/services/services.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { SocialProofComponent } from './components/social-proof/social-proof.component';
+import { LocationComponent } from './components/location/location.component';
+import { WhatsappBtnComponent } from './components/whatsapp-btn/whatsapp-btn.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, HeroComponent, ServicesComponent, FooterComponent, SocialProofComponent, 
+    LocationComponent, WhatsappBtnComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/components/social-proof/social-proof.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-social-proof',
+  standalone: true,
+  imports: [],
+  templateUrl: './social-proof.component.html',
+  styleUrl: './social-proof.component.css'
+})
+export class SocialProofComponent {
+
+}
