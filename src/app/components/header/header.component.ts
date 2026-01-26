706471a7136959671a817d112f71be0a721b9209
Mateus Rodrigues Gomes <mateus.rg01@gmail.com>
2026-01-26 17:41:18 -0300

    Alterando visual de precos e headers

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,22 +1,32 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common'; // <--- Importante para funcionar o clique
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule], // <--- Adicione aqui
+  imports: [CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
   
-  isMenuOpen = false; // Começa fechado
+  isMenuOpen = false;
 
   toggleMenu() {
-    this.isMenuOpen = !this.isMenuOpen; // Se tá fechado, abre. Se tá aberto, fecha.
+    this.isMenuOpen = !this.isMenuOpen;
   }
 
-  closeMenu() {
-    this.isMenuOpen = false; // Garante que fecha ao clicar num link
+  // --- FUNÇÃO NOVA AQUI ---
+  scrollTo(id: string) {
+    this.isMenuOpen = false; // 1. Fecha o menu na hora
+    
+    // 2. Procura a sessão (home, precos, etc) e rola até ela
+    const element = document.getElementById(id);
+    if (element) {
+      // Dá um tempinho minúsculo pro menu fechar antes de rolar
+      setTimeout(() => {
+        element.scrollIntoView({ behavior: 'smooth', block: 'start' });
+      }, 100);
+    }
   }
 }
